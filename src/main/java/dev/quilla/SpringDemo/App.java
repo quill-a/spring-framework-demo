15a0cad68f19a2841e8fba433b44ce3bb30eaa3a
quill-a <gabesander@gmail.com>
2025-07-27 20:59:47 -0500

    Remove deprecated dependencies

--- src/main/java/dev/quilla/SpringDemo/App.java
+++ src/main/java/dev/quilla/SpringDemo/App.java
@@ -1,14 +1,18 @@
 package dev.quilla.SpringDemo;
 
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
   
   public static void main(String[] args) {
     
-    BeanFactory factory = new ClassPathXmlApplicationContext("spring.xsd");
+    //BeanFactory factory = new ClassPathXmlApplicationContext("spring.xsd");
+    
+    ApplicationContext factory = new ClassPathXmlApplicationContext("spring.xml");
     
     Alien obj = (Alien) factory.getBean("alien");
+    obj.code();
   }
 }
