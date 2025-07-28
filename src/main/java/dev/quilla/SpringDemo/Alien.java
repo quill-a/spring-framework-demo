7fb02b60be2df9942564cc82b88bd6cf953207c0
quill-a <gabesander@gmail.com>
2025-07-27 21:18:39 -0500

    Add constructor

--- src/main/java/dev/quilla/SpringDemo/Alien.java
+++ src/main/java/dev/quilla/SpringDemo/Alien.java
@@ -1,6 +1,11 @@
 package dev.quilla.SpringDemo;
 
 public class Alien {
+  
+  public Alien() {
+    System.out.println("Alien Object Created.");
+  }
+  
 	public void code() {
 		System.out.println("I'm Coding!");
 	}
