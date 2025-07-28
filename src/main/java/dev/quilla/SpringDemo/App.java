f00dc0f2301540d559bcdd085ec22d35bea570f0
quill-a <gabesander@gmail.com>
2025-07-27 19:16:15 -0500

    Add XML for Spring config

--- src/main/java/dev/quilla/SpringDemo/App.java
+++ src/main/java/dev/quilla/SpringDemo/App.java
@@ -1,8 +1,14 @@
 package dev.quilla.SpringDemo;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
-    public static void main(String[] args) {
-        Alien obj = getBean();
-    }
+  
+  public static void main(String[] args) {
+    
+    BeanFactory factory = new ClassPathXmlApplicationContext("spring.xsd");
+    
+    Alien obj = (Alien) factory.getBean("alien");
+  }
 }
