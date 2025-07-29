cd141eebde649a7fc7ceb44725ebb243e1b5e4c4
quill-a <gabesander@gmail.com>
2025-07-28 20:44:45 -0500

    Practice Setter Injection

--- src/main/java/dev/quilla/SpringDemo/Alien.java
+++ src/main/java/dev/quilla/SpringDemo/Alien.java
@@ -2,12 +2,21 @@ package dev.quilla.SpringDemo;
 
 public class Alien {
   
-  int age;
+  private int age;
   
   public Alien() {
     System.out.println("Alien Object Created.");
   }
   
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    System.out.println("Age Assigned");
+    this.age = age;
+  }
+
 	public void code() {
 		System.out.println("I'm Coding!");
 	}
--- src/main/java/dev/quilla/SpringDemo/App.java
+++ src/main/java/dev/quilla/SpringDemo/App.java
@@ -12,14 +12,8 @@ public class App {
     
     ApplicationContext factory = new ClassPathXmlApplicationContext("spring.xml");
     
-//    Alien obj1 = (Alien) factory.getBean("alien");
-//    obj1.code();
-//    obj1.age = 15;
-//    System.out.println(obj1.age);
-//    
-//    Alien obj2 = (Alien) factory.getBean("alien");
-//    obj2.code();
-//    
-//    System.out.println(obj2.age);
+    Alien obj1 = (Alien) factory.getBean("alien");
+    obj1.code();
+    System.out.println(obj1.getAge());
   }
 }
