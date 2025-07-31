136a8e6e347b24582d446131cdce0c8ac3713e8f
quill-a <gabesander@gmail.com>
2025-07-30 23:33:14 -0500

    Practice Constructor Injection

--- src/main/java/dev/quilla/SpringDemo/Alien.java
+++ src/main/java/dev/quilla/SpringDemo/Alien.java
@@ -5,6 +5,14 @@ public class Alien {
   private int age;
   private Laptop laptop;
   
+//  public Alien() {
+//    System.out.println("Alien Object Created.");
+//  }
+  
+  public Alien(int age) {
+    this.age = age;
+  }
+
   public Laptop getLaptop() {
     return laptop;
   }
@@ -20,10 +28,6 @@ public class Alien {
   public void setAge(int age) {
     System.out.println("Age Assigned");
     this.age = age;
-  }
-
-  public Alien() {
-    System.out.println("Alien Object Created.");
   }
 
 	public void code() {
